86bdb07472dc12c9f43c1479a89a84a7ef1da779
eunhee <dmsgml9903@gmail.com>
2021-09-06 12:53:23 +0900

    Functional interface, Lamda

---
+++ java8to11/src/main/java/com/example/java8to11/functionalInterface/Foo.java
@@ -0,0 +1,39 @@
+package com.example.java8to11.functionalInterface;
+
+public class Foo {
+
+    public static void main(String[] args) {
+        /* 추상 메소드 정의 */
+
+        /* 1. 익명 내부 크래스 */
+        RunSomething runSomething = new RunSomething() {
+            @Override
+            public void doIt() {
+                System.out.println("Hello");
+            }
+        };
+
+        /* 2. 익명 내부 클래스 -> 람다 */
+        RunSomething runSomething1 = () -> System.out.println("Lamda");
+
+        /* 3. 반환값이 있는 함수형 인터페이스
+        *     -> 입력값이 같으면 출력값이 달라지는 여지 만들지 않기.
+        *     -> 여지 : 1) 함수의 바깥의 값을 참조하여 쓰는 경우
+        *              2) 외부의 값을 변경하는 경우 */
+        ReturnSomething returnSomething = (num) -> {
+            return num+10;
+        };
+
+        ReturnSomething returnSomething1 = (num) -> num+10;
+
+        /* 사용 */
+        // 1.
+        runSomething.doIt();
+        // 2.
+        runSomething1.doIt();
+        // 3.
+        System.out.println(returnSomething.doIt(1));
+
+
+    }
+}
